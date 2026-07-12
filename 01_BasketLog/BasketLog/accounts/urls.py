2e4b8de286a73a118310b07bd1c4b581a19c1948
kashiwa-mo-chi <yucoco18@gmail.com>
2026-07-13 00:13:41 +0900

    投稿履歴画面完了

--- 01_BasketLog/BasketLog/accounts/urls.py
+++ 01_BasketLog/BasketLog/accounts/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import(
     RegistUserView, HomeView, UserLoginView,
-    UserLogoutView
+    UserLogoutView, mypage, history,
 )
 
 app_name = 'accounts'
@@ -10,4 +10,6 @@ urlpatterns = [
     path('regist/', RegistUserView.as_view(), name='regist'),
     path('user_login/', UserLoginView.as_view(), name='user_login'),
     path('user_logout/', UserLogoutView.as_view(), name='user_logout'),
+    path('mypage/', mypage, name='mypage'),
+    path('history/', history, name='history'),
 ]
--- 01_BasketLog/BasketLog/accounts/views.py
+++ 01_BasketLog/BasketLog/accounts/views.py
@@ -4,6 +4,10 @@ from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import reverse_lazy
 from .forms import RegistForm
 from games.models import Diary
+from django.contrib.auth.decorators import login_required
+from spots.models import ArenaFacility, ArenaNearbySpot
+
+
 
 class HomeView(TemplateView):
     template_name = 'accounts/home.html'
@@ -24,4 +28,28 @@ class UserLoginView(LoginView):
 
 class UserLogoutView(LogoutView):
     next_page = reverse_lazy('accounts:user_login')
+
+@login_required
+def mypage(request):
+    return render(request, "accounts/mypage.html")
+
+@login_required
+def history(request):
+    tab = request.GET.get("tab", "facility")
+
+    facility_posts = ArenaFacility.objects.filter(
+        user=request.user        
+    ).order_by("-created_at")
+
+    nearby_posts = ArenaNearbySpot.objects.filter(
+        user=request.user        
+    ).order_by("-created_at")
+
+    context = {
+        "tab": tab,
+        "facility_posts": facility_posts,
+        "nearby_posts": nearby_posts,
+    }
+
+    return render(request, "accounts/history.html", context)
 
