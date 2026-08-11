14e353c549325429ca837fbf59fff92346e10dd4
kashiwa-mo-chi <yucoco18@gmail.com>
2026-08-11 18:36:22 +0900

    css調整、ポートフォリオ調整

--- 01_BasketLog/BasketLog/BasketLog/urls.py
+++ 01_BasketLog/BasketLog/BasketLog/urls.py
@@ -2,12 +2,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from BasketApp import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('accounts.urls')),
     path('games/', include('games.urls')),
     path('spots/', include('spots.urls')),
+    path('portfolio/', views.portfolio, name='portfolio'),
 ]
 
 if settings.DEBUG:
